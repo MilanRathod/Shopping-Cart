4340e6929151172fc6f3de5d83b30df3b06b1383
Milan Rathod <mrmilan9.8@gmail.com>
2022-02-21 20:59:17 +0530

    Implemented JWT Authentication

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using API.Helper;
 using API.Middleware;
 using AutoMapper;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -24,16 +25,22 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAutoMapper(typeof (MappingProfiles));
+            services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                     x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.AddSingleton<IConnectionMultiplexer>(c=>{
-                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),true);
+
+            services.AddDbContext<AppIdentityDbContext>(x =>
+                    x.UseSqlite(_config.GetConnectionString("IdentityConnection")));
+
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
                 return ConnectionMultiplexer.Connect(configuration);
             });
 
             services.AddApplicationServices();
+            services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
             services
                 .AddCors(opt =>
@@ -63,6 +70,7 @@ namespace API
 
             app.UseCors("CorsPolicy");
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseSwaggerdocumentation();
